169cb0f967f41be79feb5a8b1547d0fe1eb7d2a3
Antonioneto027 <antonionet.org@gmail.com>
2025-02-15 18:08:48 -0300

    Basic login structure

--- php/index.php
+++ php/index.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-echo "Teste";
+ 
 
 ?>
 
@@ -13,10 +13,16 @@ echo "Teste";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Home Manager application">
     <meta name="author" content="Your Name">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/styles.css">
     <title>Home Manager</title>
 </head>
 <body>
-    <h1>Welcome to Home Manager</h1>
+    <h1>Login</h1>
+    <form action="login.php" method="post">
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username" required>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+        <button type="submit">Login</button>
 </body>
 </html>

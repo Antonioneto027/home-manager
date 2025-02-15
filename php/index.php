f963aab336a64165e39b3293a09ed440fae0fd8b
Antonioneto027 <antonionet.org@gmail.com>
2025-02-15 18:25:03 -0300

    Login page button added and register page sekeleton too

--- php/index.php
+++ php/index.php
@@ -1,9 +1,4 @@
-<?php 
-
-
  
-
-?>
 
 
 <!DOCTYPE html>
@@ -13,16 +8,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Home Manager application">
     <meta name="author" content="Your Name">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/styles.css">
     <title>Home Manager</title>
 </head>
-<body>
+<br>
     <h1>Login</h1>
     <form action="login.php" method="post">
         <label for="username">Username</label>
         <input type="text" id="username" name="username" required>
         <label for="password">Password</label>
         <input type="password" id="password" name="password" required>
-        <button type="submit">Login</button>
+        <button type="submit" class="btn btn-outline-primary">Login</button>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </form>
+    <br>
+    <p>Have no account? <a href="register.php" class="link-primary link-underline-opacity-0">Register</a></p> 
 </body>
 </html>

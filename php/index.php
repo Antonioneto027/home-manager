df87dec630012380924c1e4516d529720014257d
Antonioneto027 <antonionet.org@gmail.com>
2025-02-15 18:00:36 -0300

    Initial structure

---
+++ php/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+echo "Teste";
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Home Manager application">
+    <meta name="author" content="Your Name">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home Manager</title>
+</head>
+<body>
+    <h1>Welcome to Home Manager</h1>
+</body>
+</html>
